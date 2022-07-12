41c04188cb0f97e100ea289cad787a8d4027db4b
yunho <84072767+N-yun@users.noreply.github.com>
2022-07-12 15:17:52 +0900

    7_12

--- app/src/main/java/com/example/yjp_capstone/MainActivity.java
+++ app/src/main/java/com/example/yjp_capstone/MainActivity.java
@@ -4,16 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.yjp_capstone.api.ApiController;
+import com.example.yjp_capstone.databinding.ActivityMainBinding;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
 
-        // New Branch
-        // New New Branch
-        // 0630
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:9002/api/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        retrofit.create(ApiController.class);
 
     }
 }
---
+++ app/src/main/java/com/example/yjp_capstone/ui/MainFragment.java
@@ -0,0 +1,47 @@
+package com.example.yjp_capstone.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yjp_capstone.R;
+import com.example.yjp_capstone.databinding.FragmentMainBinding;
+
+public class MainFragment extends Fragment {
+    private FragmentMainBinding binding;
+
+    private MainViewModel mViewModel;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentMainBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
