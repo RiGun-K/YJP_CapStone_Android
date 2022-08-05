911940bbdcb147330ed55184fd95f003d1961b6d
KBS <84058090+sabow1@users.noreply.github.com>
2022-08-05 15:19:35 +0900

    ENDTYPE A

---
+++ app/src/main/java/com/example/yjp_capstone/viewmodel/teamplan/ResultSearchViewModel.java
@@ -0,0 +1,7 @@
+package com.example.yjp_capstone.viewmodel.teamplan;
+
+import androidx.lifecycle.ViewModel;
+
+public class ResultSearchViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
